3a4547fb0f5a4d1013166e4fd2a616d0f35c1b1f
Nathangong-coder <nagong1@g.ucla.edu>
2026-06-23 15:21:27 -0700

    hello

--- src/auth.ts
+++ src/auth.ts
@@ -1,19 +1,12 @@
 import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { prisma } from "@/lib/db"
+import { authConfig } from "@/auth.config"
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [
-    GitHub({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-  ],
-  session: {
-    strategy: "database",
-  },
+  session: { strategy: "database" },
+  ...authConfig,
   callbacks: {
     async session({ session, user }) {
       if (session.user) {
