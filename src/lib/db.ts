6a1251684f9338ea658cd0c892ff8d4058583aa9
Nathangong-coder <nagong1@g.ucla.edu>
2026-06-23 20:54:15 -0700

    bruh

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,17 +1,14 @@
 import { PrismaClient } from '@prisma/client'
 import { PrismaNeon } from '@prisma/adapter-neon'
-import { Pool } from '@neondatabase/serverless'
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined }
 
 const connectionString = process.env.DATABASE_URL
 
-const pool = new Pool({ connectionString })
-
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    adapter: new PrismaNeon(pool),
+    adapter: new PrismaNeon(connectionString),
     log: process.env.NODE_ENV === 'development' ? ['query'] : [],
   })
 
